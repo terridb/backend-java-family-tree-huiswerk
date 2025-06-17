8f5bd2eb1371acb430254b4eb3e53f9232457a5d
terridb <terrideboer99@gmail.com>
2025-06-17 18:14:42 +0200

    Pet klasse opgesteld

--- src/main/java/Pet.java
+++ src/main/java/Pet.java
@@ -1,6 +1,40 @@
 public class Pet {
     private String name;
     private int age;
-    private String species;
+    private final String species;
     private Person owner;
+
+    public Pet(String species, int age, String name) {
+        this.species = species;
+        this.age = age;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public Person getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Person owner) {
+        this.owner = owner;
+    }
 }
